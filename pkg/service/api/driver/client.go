6e12b27a3733b7eb8f54781d50be3afe94a071a1
neil <esz759486@gmail.com>
2021-08-17 23:58:29 +0800

    pkg/service/api/driver/client_test show http Get body

--- pkg/service/api/driver/client.go
+++ pkg/service/api/driver/client.go
@@ -25,7 +25,6 @@ func Get(url string, body io.Reader) *http.Response {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer response.Body.Close()
 
 	return response
 }
--- pkg/service/api/driver/client_test.go
+++ pkg/service/api/driver/client_test.go
@@ -2,15 +2,17 @@ package driver
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
 	"testing"
 )
 
 func TestGet(t *testing.T) {
-	response := Get("https://localhost")
-	body, _ := io.ReadAll(response.Body)
-
-	fmt.Printf("response:%v\n", string(body))
+	response := Get("https://data.ntpc.gov.tw/api/datasets/308DCD75-6434-45BC-A95F-584DA4FED251/json/preview", nil)
+	defer response.Body.Close()
 	t.Log(response)
+	body, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Printf("response:%v\n", body)
+	t.Log(string(body))
 
 }
