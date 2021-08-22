62d15d46a9cf8736b23d305437ba1c5739983572
neil <esz759486@gmail.com>
2021-08-23 00:45:28 +0800

    update pkg/helper/file.go CheckDirectoryExists

--- pkg/helper/file.go
+++ pkg/helper/file.go
@@ -2,7 +2,13 @@ package helper
 
 import (
 	"mime"
+	"os"
 )
+
+func CheckDirectoryExists(dir string) bool {
+	_, err := os.Stat(dir)
+	return !os.IsNotExist(err)
+}
 
 func GetFileContentType(extension string) string {
 	return mime.TypeByExtension(extension)
