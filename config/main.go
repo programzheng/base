4b17ce67dded33d00cbcc57595f327fafbe1d3bd
neil <esz759486@gmail.com>
2021-08-23 00:43:21 +0800

    fix log directory error

--- config/main.go
+++ config/main.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 
 	//use this get .env setting
 	"github.com/spf13/viper"
@@ -20,7 +20,7 @@ func setTimeZone() {
 	TZ := viper.Get("TZ").(string)
 	local, err := time.LoadLocation(TZ)
 	if err != nil {
-		logrus.Error("set timezone error:", err)
+		log.Error("set timezone error:", err)
 	}
 	time.Local = local
 }
@@ -29,15 +29,22 @@ func setLog() {
 	system := viper.Get("LOG_SYSTEM").(string)
 	switch system {
 	case "file":
-		path := viper.Get("LOG_PATH").(string) + "/"
-		fileName := time.Now().Format("20060102") + ".log"
-		file, err := os.OpenFile(path+fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err == nil {
-			logrus.SetOutput(file)
-		} else {
-			logrus.Info("Failed to log to file, using default stderr")
+		path := "." + viper.Get("LOG_PATH").(string) + "/"
+		//check log path directory exist
+		_, err := os.Stat(path)
+		if os.IsNotExist(err) {
+			err = os.Mkdir(path, 0644)
+			if err != nil {
+				log.Fatal("cerate log directory error:", err)
+			}
 		}
-		logrus.Info("log service running")
+		fileName := time.Now().Format("20060102") + ".log"
+		file, err := os.OpenFile(path+fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			log.Info("Failed to log to file, using default stderr", err)
+		}
+		log.SetOutput(file)
+		log.Info("log service running")
 	}
 }
 
