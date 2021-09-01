8aea8debbb9891e4ae9fc2b9af6909c9dd097e93
programzheng <esz759486@gmail.com>
2021-09-01 23:45:20 +0800

    調整line_user func UserParseTextGenTemplate return text方法

--- pkg/service/bot/line_user.go
+++ pkg/service/bot/line_user.go
@@ -3,6 +3,8 @@ package bot
 import (
 	"base/pkg/library/line/bot/template"
 	"strings"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func UserParseTextGenTemplate(lineId LineID, text string) interface{} {
@@ -26,13 +28,13 @@ func UserParseTextGenTemplate(lineId LineID, text string) interface{} {
 		case "every":
 			// TODO|every 19:55|測試29號13:30送出
 			todoAction(lineId.UserID, "every", parseDate[1], template.TODO(replyText))
-			return template.Text("設置完成將於每天" + parseDate[1] + "\n傳送訊息:" + replyText)
+			return linebot.NewTextMessage("設置完成將於每天" + parseDate[1] + "\n傳送訊息:" + replyText)
 		default:
 			// TODO|2020/02/29 13:00|測試29號13:30送出
 			todoAction(lineId.UserID, "once", date, template.TODO(replyText))
-			return template.Text("設置完成將於" + date + "\n傳送訊息:" + replyText)
+			return linebot.NewTextMessage("設置完成將於" + date + "\n傳送訊息:" + replyText)
 		}
 
 	}
-	return template.Text(text)
+	return linebot.NewTextMessage(text)
 }
