bb5ca2e762012ce1ab7589542066b6801a69f54f
programzheng <esz759486@gmail.com>
2021-10-16 03:52:02 +0800

    .env.example add redis, redis_test

--- pkg/cache/redis_test.go
+++ pkg/cache/redis_test.go
@@ -2,23 +2,69 @@ package cache
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"testing"
+	"time"
 )
 
 var ctx = context.Background()
 
-func TestSet(t *testing.T) {
+func TestSetString(t *testing.T) {
 	err := Rdb.Set(ctx, "test", "foo", 0).Err()
 	if err != nil {
 		panic(err)
 	}
 }
 
-func TestGet(t *testing.T) {
+func TestGetString(t *testing.T) {
 	val, err := Rdb.Get(ctx, "test").Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("value:", val)
+	log.Fatalf("value:%v\n", val)
+}
+
+func TestSAdd(t *testing.T) {
+	err := Rdb.SAdd(ctx, "set", 2, 1).Err()
+	if err != nil {
+		panic(err)
+	}
+	err = Rdb.Expire(ctx, "set", 5*time.Second).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestHSet(t *testing.T) {
+	err := Rdb.HSet(ctx, "test001", "action", "add").Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestHGet(t *testing.T) {
+	action, err := Rdb.HGet(ctx, "test001", "action").Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Fatalf("action:%v", action)
+}
+
+func TestSGet(t *testing.T) {
+	err := Rdb.SAdd(ctx, "set", "foo", 2, 1).Err()
+	if err != nil {
+		panic(err)
+	}
+	err = Rdb.Expire(ctx, "set", 5*time.Second).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSMember(t *testing.T) {
+	es, err := Rdb.SMembers(ctx, "set").Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Fatalf("%v\n", es)
 }
