7c731cee5eaad8b3a6b7d2f4d83ac72af8fa216b
programzheng <esz759486@gmail.com>
2022-02-19 16:49:00 +0800

    modify: remove fmt.Println(gin.Mode())

--- server/http/router/router.go
+++ server/http/router/router.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,7 +9,6 @@ func SetRouter(router *gin.Engine) {
 	setMiddleware(router)
 	// 設置Web Route
 	setRoute(router)
-	fmt.Println(gin.Mode())
 	if mode := gin.Mode(); mode != gin.ReleaseMode {
 		// 測試頁面 Route
 		setTestRoute(router)
