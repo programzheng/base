46db1294599155dcdbcc323d2def5a38b33eb9c0
programzheng <programzheng@gmail.com>
2022-07-30 22:59:34 +0800

    :fire:

--- migrations/20220727000027_users.sql
+++ migrations/20220727000027_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE `users` (
     `id` int unsigned NOT NULL AUTO_INCREMENT,
     `created_at` timestamp NULL DEFAULT NULL,
     `updated_at` timestamp NULL DEFAULT NULL,
-    `deleted_at` timestamp NULL DEFAULT NULL,https://zh.wikipedia.org/zh-tw/%E9%80%9A%E7%94%A8%E5%94%AF%E4%B8%80%E8%AF%86%E5%88%AB%E7%A0%81
+    `deleted_at` timestamp NULL DEFAULT NULL,
     `uuid` varchar(36) NOT NULL,
     `account` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
